374aa80e33277ef6e2df46d93785e882b7221690
BISWAJIT MANDAL <34375403+Biswajit1729@users.noreply.github.com>
2023-01-27 14:24:48 +0530

    Update livekitToken.js

--- livekitToken.js
+++ livekitToken.js
@@ -3,6 +3,7 @@ const AccessToken = livekitApi.AccessToken;
 
 
 async function betaToken(roomName, participantName) {
+    console.log(roomName, participantName);
     const at = new AccessToken('APIivisjvnBSpea', 'NwF9luIhxy6NuEiBGpGC4RlReu30v1KFPnLKuayIOOa', {
         identity: participantName.toString(),
     });
